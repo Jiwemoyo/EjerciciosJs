48e10d3c0a609277c659d37f25d0846dd48b2067
Alessandro Cisneros <jiwemoyo@gmail.com>
2022-02-23 19:37:13 -0500

    Hice el ejercicio 2 en js y cambie el nombre de los ejercicio 1 y 2

---
+++ js/ejercicio02.js
@@ -0,0 +1,7 @@
+// Realizar un programa en el que se ingresen los datos de un trabajador y nos de como resultado el sueldo que debe obtener en base a sus horas trabajadas
+let nombre =prompt("Ingrese el nombre del trabajador: ")
+let horasTrabajadas = prompt("Horas trabajadas al mes?")
+let valorHora =prompt("Cuanto gana por hora?")
+sueldo =parseInt(horasTrabajadas)*parseInt(valorHora)
+
+document.write(`El trabajador ${nombre} debe recibir una remuneracion de ${sueldo}`)
