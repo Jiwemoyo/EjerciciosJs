59a737903a77fc7da6d73aa28c31b3e75fbbac8f
Alessandro Cisneros <jiwemoyo@gmail.com>
2022-02-23 19:25:42 -0500

    Hice ele ejercicio 1 en js

---
+++ js/ejercicio1.js
@@ -0,0 +1,6 @@
+// Ingrese dos números y realice una suma con los mismos. 
+let val1=prompt("Ingrese el valor 1")
+let val2=prompt("Ingrese el valor 2")
+let suma = parseInt(val1) + parseInt(val2)
+document.write(`La suma de: ${val1} y ${val2} es ${suma}`)
+
