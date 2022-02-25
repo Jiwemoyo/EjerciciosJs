3ea5e5fa1989222e5a67e37cd5607aa750f1fcbe
Alessandro Cisneros <jiwemoyo@gmail.com>
2022-02-24 20:29:29 -0500

    Realice el ejercicio 6

---
+++ js/ejercicio06.js
@@ -0,0 +1,29 @@
+//Si es vocal o no lo es
+let letra = prompt("Dime una letra para saber si es vocal o no")
+
+switch (letra) {
+    case "a":
+    case "A":
+        alert("Es una vocal")
+        break;
+    case "e":
+    case "E":
+        alert("Es una vocal")
+        break;
+    case "i":
+    case "I":
+        alert("Es una vocal")
+        break;
+    case "o":
+    case "O":
+        alert("Es una vocal")
+        break;
+    case "u":
+    case "U":
+        alert("Es una vocal")
+        break;
+
+    default:
+        alert("Es una consonante")
+        break;
+}
