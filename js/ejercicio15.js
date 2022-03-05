7490e86256216df02a2d363b27faa8d354ffe859
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2022-03-05 10:08:42 -0500

    ejercicio 15 terminado

--- js/ejercicio15.js
+++ js/ejercicio15.js
@@ -11,14 +11,19 @@
 	imp = 0;
 	for (contador=1;contador<=10;contador++) {
 		
-		num = Number(prompt("Ingrese un numero de un total de 10"));
+		num = Number(prompt(`Ingrese el ${contador} numero de un total de 10`));
 		if (num%2==0) {
 			parr = parr+1;
 		} else {
 			imp = imp+1;
 		}
 	}
-	document.write("la cantidad de numeros pares ingresados es de: ",parr,'<BR/>');
-	document.write("***********************************************",'<BR/>');
-	document.write("la cantidad de numero impares es de: ",imp,'<BR/>');
-	
+
+	var par=document.getElementById("par")
+	var impar=document.getElementById("Impar")
+
+	par.innerHTML=parr
+	impar.innerHTML=imp 
+
+
+
