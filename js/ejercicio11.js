90b9345fba4a107fc3b315ab4ab948c9ea121c9a
AngelesQuinatoa <nagelyquinatoa3@gmail.com>
2022-03-05 14:20:23 -0600

    ANGELES QUINATOA
    css

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -2,7 +2,7 @@ let anio;
 anio = prompt("Ingrese el año");
 anio = parseInt(anio)
 if((anio%4==0) && (anio%100  !=0 || anio%400==0)) {
- document.write("El año es bisiesto");
+ document.getElementById("di").innerHTML=("Si es bisiesto");
 }else{
-    document.write("El año no es bisiesto");
+    document.getElementById("di").innerHTML=("No es bisiesto");
 }
