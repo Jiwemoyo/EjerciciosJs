ca0bba40c5c5603540d00b3894c5fbb2d30e3fe8
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2022-03-05 11:28:32 -0500

    Ejercicio 18 completado

--- js/ejercicio18.js
+++ js/ejercicio18.js
@@ -3,13 +3,14 @@
 	var nmayor = new Number();
 	var num = new Number();
 	var i = new Number();
-	document.write("Ingrese 10 numeros",'<BR/>');
+	
 	nmayor = 0;
 	for (i=1;i<=10;i++) {
-		num = Number(prompt("Ingrese 10 numeros"));
+		num = Number(prompt(`Ingrese ${i}º de 10 numeros`));
 		if (nmayor<num) {
 			nmayor = num;
 		}
 	}
-	document.write("El numero mayo de los 10 digitos es: ",nmayor,'<BR/>');
+	document.getElementById("numMay").innerHTML=`El mayor de los diez numeros ingresados es: ${nmayor}`
+	
 	
