fa940f04242207190cb625e6b76299970eb8ef69
Fer :3 <97122618+JimmyLorente@users.noreply.github.com>
2022-03-05 21:01:44 -0500

    jasha

--- js/ejercicio29.js
+++ js/ejercicio29.js
@@ -1,16 +1,22 @@
+// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
 
-	/*var c, f, serie;
-	for (f=1;f<=9;f++) {
-		serie = "";
-		for (c=1;c<=10-f;c++) {
-			serie = serie+" "+String(c);
-		}
-		document.write("6",'<BR/>');
-	}*/
-    document.write("N:6",'<BR/>');
-    document.write("123456",'<BR/>');
-    document.write("12345",'<BR/>');
-    document.write("1234",'<BR/>');
-    document.write("123",'<BR/>');
-    document.write("12",'<BR/>');
-    document.write("1",'<BR/>');
+function desglosamiento_de_numeros() {
+	var cifra, digito, num;
+	// Alessandro Cisneros, Ángeles Quinatoa, Jimmy Lorente, Brandon Caranqui
+	// hecho por: Quinatoa Nagely
+	// REALIZAR UN PROGRAMA QUE MUESTRE LA SIGUIENTE SERIE NUMERICA GRAFICA:
+	document.write("Escriba un número matoy de 3 cifras",'<BR/>');
+	num = prompt();
+	document.write("-----------------",'<BR/>');
+	cifra = num;
+	document.write(num,'<BR/>');
+	document.write("-----------------",'<BR/>');
+	while (cifra>1) {
+		digito = cifra%10;
+		cifra = (cifra-digito)/10;
+		document.write(" cifra : ",cifra,'<BR/>');
+	}
+}
+
