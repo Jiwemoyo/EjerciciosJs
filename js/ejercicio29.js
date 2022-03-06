69e9d932890f42425710defacfab4ee867d057ef
Fer :3 <97122618+JimmyLorente@users.noreply.github.com>
2022-03-05 21:15:42 -0500

    jahjah

--- js/ejercicio29.js
+++ js/ejercicio29.js
@@ -1,14 +1,8 @@
-// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
-// Es posible que el codigo generado no sea completamente correcto. Si encuentra
-// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
 
-function desglosamiento_de_numeros() {
 	var cifra, digito, num;
-	// Alessandro Cisneros, Ángeles Quinatoa, Jimmy Lorente, Brandon Caranqui
-	// hecho por: Quinatoa Nagely
-	// REALIZAR UN PROGRAMA QUE MUESTRE LA SIGUIENTE SERIE NUMERICA GRAFICA:
-	document.write("Escriba un número matoy de 3 cifras",'<BR/>');
-	num = prompt();
+	
+	document.write("Escriba un numero mayor de 3 cifras",'<BR/>');
+	num = prompt("Escriba un numero mayor de 3 cifras");
 	document.write("-----------------",'<BR/>');
 	cifra = num;
 	document.write(num,'<BR/>');
@@ -18,5 +12,4 @@ function desglosamiento_de_numeros() {
 		cifra = (cifra-digito)/10;
 		document.write(" cifra : ",cifra,'<BR/>');
 	}
-}
 
