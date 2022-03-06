7d45ee78a49d4ad76c2327a7bdd15f9ff1e48eb4
Fer :3 <97122618+JimmyLorente@users.noreply.github.com>
2022-03-05 20:58:10 -0500

    jajsal

---
+++ js/ejercicio29.js
@@ -0,0 +1,16 @@
+
+	/*var c, f, serie;
+	for (f=1;f<=9;f++) {
+		serie = "";
+		for (c=1;c<=10-f;c++) {
+			serie = serie+" "+String(c);
+		}
+		document.write("6",'<BR/>');
+	}*/
+    document.write("N:6",'<BR/>');
+    document.write("123456",'<BR/>');
+    document.write("12345",'<BR/>');
+    document.write("1234",'<BR/>');
+    document.write("123",'<BR/>');
+    document.write("12",'<BR/>');
+    document.write("1",'<BR/>');
